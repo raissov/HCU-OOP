846e57005787b07f5cd7387f61d1f8260a1fc0ba
raissov <brown.robert@bk.ru>
2021-01-18 20:58:14 +0600

    added: cylinder subcalss

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -2,13 +2,20 @@ package com.company;
 
 public class Circle {
     private double radius;
+    private String color;
 
     public Circle(){
         radius = 1.0;
+        color = "red";
     }
 
     public Circle(double radius) {
         this.radius = radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
     }
 
     public double getRadius() {
@@ -17,6 +24,14 @@ public class Circle {
 
     public void setRadius(double radius) {
         this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public double getArea(){
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        Cylinder a = new Cylinder(5,"red",10);
+        System.out.println(a.getVolume());
     }
 }
